cb4d88ccaa0ebf620655a825c264cd12bd9931b6
Harshith <harshithdr10@gmail.com>
2023-09-17 05:05:49 -0500

    update button

--- test1.py
+++ test1.py
@@ -7,9 +7,21 @@ window.title('password manager')
 window.geometry('600x400')
 
 #widgets
-label = ctk.CTkLabel(window, text = 'label')
+label = ctk.CTkLabel(
+    window,
+    text = 'Label',
+    text_color = 'red',
+    fg_color = 'white',
+    corner_radius = 12)
 label.pack()
 
+#button
+button = ctk.CTkButton(window,
+                       text = 'click here',
+                       fg_color="#DB3E39",
+                       hover_color= "#821D1A",
+                       command = lambda: print('button pressed'))
+button.pack()
 
 #run
 window.mainloop()
