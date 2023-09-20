7f3fcd6f8f0ff08bf7b95758b66c1588f111d234
Harshith <harshithdr10@gmail.com>
2023-09-20 04:43:17 -0500

    working home page and password page

--- working code/home_page.py
+++ working code/home_page.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import encryptor
 
+
 class Home_test:
     def __init__(self, window, show_gp_test_callback_fun):
         self.window = window
@@ -10,13 +11,12 @@ class Home_test:
         self.home_test_frame = ctk.CTkFrame(master=window,
                                             corner_radius=20)
 
-
         # creating input area
         self.label = ctk.CTkLabel(master=self.home_test_frame,
-                             text="Enter the keys to generate password",
-                             font=('TkHeadingFont', 15, "bold"),
-                             pady=10,
-                             text_color='#FF7900')
+                                  text="Enter the keys to generate password",
+                                  font=('TkHeadingFont', 15, "bold"),
+                                  pady=10,
+                                  text_color='#FF7900')
 
         # creating input area
         self.entry_1 = ctk.CTkEntry(self.home_test_frame, placeholder_text="key 1")
@@ -27,15 +27,14 @@ class Home_test:
         self.submit_button = ctk.CTkButton(self.home_test_frame,
                                            text="Submit",
                                            command=self.onSubmit)
-    
 
-        #packing everything
+        # packing everything
         self.label.pack()
         self.entry_1.pack(padx=30, pady=10)
         self.entry_2.pack(padx=10, pady=10)
         self.entry_3.pack(padx=20, pady=10)
         self.submit_button.pack(pady=10)
-        
+
     def generatePassword(self):
         self.key1 = self.entry_1.get()
         self.key2 = self.entry_2.get()
@@ -43,7 +42,7 @@ class Home_test:
         self.password = encryptor.encrypt_string(self.key1 + self.key2 + self.key3)
         print(self.password)
         return self.password
-        
+
     def onSubmit(self):
         self.genpassword = self.generatePassword()
         self.home_test_frame.pack_forget()
