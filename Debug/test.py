cd2e056ba0d7bfeb2ce26b87369cdc0bed419923
Harshith <harshithdr10@gmail.com>
2023-09-29 23:58:43 -0500

    add hide password

--- Debug/test.py
+++ Debug/test.py
@@ -1,17 +1,15 @@
 import customtkinter as ctk
 import encryptor
 
-
 class Home_test:
     def __init__(self, window, show_gp_test_callback_fun):
         self.window = window
         self.show_gp_test_callback_fun = show_gp_test_callback_fun
 
         # creating home screen frame
-        self.home_test_frame = ctk.CTkFrame(master=window,
-                                            corner_radius=20)
+        self.home_test_frame = ctk.CTkFrame(master=window, corner_radius=20)
 
-        # creating label
+        # creating input area
         self.home_label = ctk.CTkLabel(master=self.home_test_frame,
                                        text="Enter the keys to generate password",
                                        font=('TkHeadingFont', 15, "bold"),
@@ -19,24 +17,18 @@ class Home_test:
                                        text_color='#FF7900')
 
         # creating input area
-        self.entry_1 = ctk.CTkEntry(self.home_test_frame, placeholder_text="Master Key", show = '*')
-        self.entry_2 = ctk.CTkEntry(self.home_test_frame, placeholder_text="Number", show = '*')
-        self.entry_3 = ctk.CTkEntry(self.home_test_frame, placeholder_text="Application", show = '*')
+        self.entry_1 = ctk.CTkEntry(self.home_test_frame, placeholder_text="Master Key", show='•')
+        self.entry_2 = ctk.CTkEntry(self.home_test_frame, placeholder_text="Number", show='•')
+        self.entry_3 = ctk.CTkEntry(self.home_test_frame, placeholder_text="Application", show='•')
 
-        # checkboxs
-        self.check_Uppercase = ctk.StringVar(value="on")
-        self.checkbox_upper = ctk.CTkCheckBox(self.home_test_frame,
-                                              text="Include Uppercase",
-                                              variable=self.check_Uppercase,
-                                              onvalue="on",
-                                              offvalue="off")
-
-        self.check_special_characters = ctk.StringVar(value="on")
-        self.checkbox_special = ctk.CTkCheckBox(self.home_test_frame,
-                                                text="Include Special Case",
-                                                variable=self.check_special_characters,
-                                                onvalue="on",
-                                                offvalue="off")
+        # checkbox to show/hide password
+        self.show_password_var = ctk.StringVar(value=False)
+        self.show_password_checkbox = ctk.CTkCheckBox(self.home_test_frame,
+                                                      text="Show Password",
+                                                      variable=self.show_password_var,
+                                                      onvalue=True,
+                                                      offvalue=False,
+                                                      command=self.toggle_password_visibility)
 
         # Create a Submit button
         self.submit_button = ctk.CTkButton(self.home_test_frame,
@@ -48,9 +40,16 @@ class Home_test:
         self.entry_1.pack(padx=30, pady=10)
         self.entry_2.pack(padx=10, pady=10)
         self.entry_3.pack(padx=20, pady=10)
-        self.checkbox_upper.pack(padx=10, pady=10)
-        self.checkbox_special.pack(padx=10, pady=10)
+        self.show_password_checkbox.pack(padx=10, pady=10)
         self.submit_button.pack(pady=10)
+
+    def toggle_password_visibility(self):
+        # Toggle password visibility based on checkbox state
+        show_password = self.show_password_var.get()
+        show_char = '' if show_password else '•'
+        self.entry_1.configure(show=show_char)
+        self.entry_2.configure(show=show_char)
+        self.entry_3.configure(show=show_char)
 
     def generatePassword(self):
         self.key1 = self.entry_1.get()
@@ -58,7 +57,6 @@ class Home_test:
         self.key3 = self.entry_3.get()
 
         self.password = encryptor.encrypt_string(self.key1 + self.key2 + self.key3)[7:23]
-        # print(self.password)
 
         if self.checkbox_upper.get() == 'on':
             self.password = self.password + 'A'
